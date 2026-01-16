c4750fc8476a65eaa94cc956a4b503811d6f0035
cdailaincao <86619935+cdailaincao@users.noreply.github.com>
2026-01-16 09:57:24 +0530

    Update WellnessController.java

--- src/main/java/com/serenity/controller/WellnessController.java
+++ src/main/java/com/serenity/controller/WellnessController.java
@@ -1,1 +1,34 @@
+package com.serenity.controller;
+
+import com.serenity.model.WellnessData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class WellnessController {
+
+    private WellnessData data = new WellnessData();
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("data", data);
+        return "index";
+    }
+
+    @PostMapping("/save")
+    public String saveData(
+            @RequestParam String mood,
+            @RequestParam String food,
+            @RequestParam int water,
+            @RequestParam double sleep
+    ) {
+        data.setMood(mood);
+        data.setFood(food);
+        data.setWater(water);
+        data.setSleep(sleep);
+
+        return "redirect:/";
+    }
+}
 
