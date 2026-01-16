c8838540932153e37ab7738fff2a599aa4565397
cdailaincao <86619935+cdailaincao@users.noreply.github.com>
2026-01-16 09:58:51 +0530

    Update WellnessData.java

--- src/main/java/com/serenity/model/WellnessData.java
+++ src/main/java/com/serenity/model/WellnessData.java
@@ -1,1 +1,41 @@
+package com.serenity.model;
 
+public class WellnessData {
+
+    private String mood;
+    private String food;
+    private int water;
+    private double sleep;
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public void setWater(int water) {
+        this.water = water;
+    }
+
+    public double getSleep() {
+        return sleep;
+    }
+
+    public void setSleep(double sleep) {
+        this.sleep = sleep;
+    }
+}
